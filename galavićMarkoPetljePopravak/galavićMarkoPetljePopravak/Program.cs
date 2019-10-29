a2a3be6b80fb7c83fc705b782b37135a3f1eb0f1
markogalavic <55134792+markogalavic@users.noreply.github.com>
2019-10-29 20:10:22 +0100

    Add files via upload

---
+++ galavićMarkoPetljePopravak/galavićMarkoPetljePopravak/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galavićMarkoPetljePopravak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int ocijena;
+
+            Console.WriteLine("Unesi ocijenu");
+
+            ocijena = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(ocijena);
+
+            for (int i = 0; i<6; i++)
+            {
+                if (ocijena > 0 || ocijena < 6) {
+                    ocijena = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine(ocijena);
+                }
+                if (ocijena == 1)
+                {
+                    Console.WriteLine("Prosjek je 1");
+                }
+                
+              
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
